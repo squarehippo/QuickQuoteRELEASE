d2f2f0ccb81402049ce817454787c8a808ffa1ec
Brian Wilson <squarehippobills@gmail.com>
2019-10-14 19:29:50 -0400

    Initial Commit

---
+++ QuickQuote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickQuote
+//
+//  Created by Brian Wilson on 10/14/19.
+//  Copyright © 2019 Brian Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
