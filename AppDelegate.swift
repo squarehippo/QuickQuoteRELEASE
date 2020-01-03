4c4485848886af413e97931188ee31f99b9dee57
Brian Wilson <squarehippobills@gmail.com>
2020-01-03 15:04:48 -0500

    Switching to inject3ed context

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -32,28 +32,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
         let container = NSPersistentContainer(name: "QuickQuote")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
@@ -62,19 +45,68 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data Saving support
 
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-
+    func saveContext(completion: (_ success: Bool) -> Void) {
+           if persistentContainer.viewContext.hasChanges {
+               for object in persistentContainer.viewContext.updatedObjects {
+                   if let signatureObject = object as? SignatureManagedObject {
+                       signatureObject.sign()
+                   }
+               }
+               
+               for object in persistentContainer.viewContext.insertedObjects {
+                   if let signatureObject = object as? SignatureManagedObject {
+                       signatureObject.sign()
+                   }
+               }
+               do {
+                   try persistentContainer.viewContext.save()
+                   completion(true)
+               } catch {
+                    fatalError("Error saving main managed context \(error)")
+               }
+           }
+       }
+       
+       func saveContext() {
+           if persistentContainer.viewContext.hasChanges {
+               for object in persistentContainer.viewContext.updatedObjects {
+                   if let signatureObject = object as? SignatureManagedObject {
+                       signatureObject.sign()
+                   }
+               }
+               
+               for object in persistentContainer.viewContext.insertedObjects {
+                   if let signatureObject = object as? SignatureManagedObject {
+                       signatureObject.sign()
+                   }
+               }
+               do {
+                   try persistentContainer.viewContext.save()
+               } catch {
+                   fatalError("Error saving main managed context \(error)")
+               }
+           }
+       }
+    
+    
+    
+    
+//    func saveContext () {
+//        let context = persistentContainer.viewContext
+//        if context.hasChanges {
+//            do {
+//                try context.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nserror = error as NSError
+//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//            }
+//        }
+//    }
+//
+//
+//
+    
 }
 
