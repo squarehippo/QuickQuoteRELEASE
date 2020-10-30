be073a4eb50aba47cf0ea0970394f152c022ce93
Brian Wilson <squarehippobills@gmail.com>
2020-10-30 13:54:35 -0400

    v2.53 - adjusting cover sheet fonts

---
+++ QuickQuote/View/PDFPage1View.swift
@@ -0,0 +1,21 @@
+//
+//  PDFPage1View.swift
+//  QuickQuote
+//
+//  Created by Brian Wilson on 10/11/20.
+//  Copyright © 2020 Brian Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct PDFPage1View: View {
+    var body: some View {
+        Text("Hello!")
+    }
+}
+
+struct PDFPage1View_Previews: PreviewProvider {
+    static var previews: some View {
+        PDFPage1View()
+    }
+}
